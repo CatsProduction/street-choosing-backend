974c1f0c40802a264ccd46f7241710627345d52e
Nadia Shokel <primisen17@gmail.com>
2023-01-30 18:01:55 +0300

    Init DB

---
+++ src/main/resources/db/migration/V202301301602__init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS city
+(
+    id          SERIAL          PRIMARY KEY,
+    name        VARCHAR(50)     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS street
+(
+    id          SERIAL          PRIMARY KEY,
+    city_id     INT             NOT NULL,
+    name        VARCHAR(100)    NOT NULL,
+    visited     BOOLEAN         DEFAULT FALSE
+);
