05e7744ca1d9f17df4370766f9ecd33e0ec92e2d
Nadia Shokel <primisen17@gmail.com>
2023-01-31 17:46:24 +0300

    Added StreetController class

---
+++ src/main/java/com/cats_production/street_choosing/controller/StreetController.java
@@ -0,0 +1,27 @@
+package com.cats_production.street_choosing.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/streets")
+public class StreetController {
+
+    @Autowired
+    private StreetService streetService;
+
+
+    @GetMapping("/")
+    public Street getRandomStreet(){
+        return streetService.getRandomStreet();
+    }
+
+    @PutMapping("/{id}")
+    public void setStreetIsVisited(@PathVariable Integer id){
+        streetService.setStreetIsVisited(id);
+    }
+}
